578406a374439880642c7dd2dfd5c12a66be0d55
yutwka <matheus.tuji@sptech.school>
2026-09-10 15:24:43 -0300

    feat: add bad request exception

--- src/main/kotlin/mercado_livro/model/BookModel.kt
+++ src/main/kotlin/mercado_livro/model/BookModel.kt
@@ -10,6 +10,8 @@ import jakarta.persistence.Id
 import jakarta.persistence.JoinColumn
 import jakarta.persistence.ManyToOne
 import mercado_livro.enums.BookStatus
+import mercado_livro.enums.Errors
+import mercado_livro.exception.BadRequestException
 import java.math.BigDecimal
 
 @Entity(name = "book")
@@ -36,7 +38,7 @@ data class BookModel(
     var status: BookStatus? = null
         set(value) {
             if(field == BookStatus.CANCELADO || field == BookStatus.DELETADO)
-                throw Exception("Não é possível alterar um livro com status ${field}")
+                throw BadRequestException(Errors.ML102.message.format(field), Errors.ML102.code)
 
             field = value
         }
